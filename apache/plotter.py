12de0729bab564738320ec7f705a75f3d4c648ae
William Shen <shenpaiwilliam@gmail.com>
2023-12-09 23:02:12 -0800

    Basic plotter for apache requests

--- apache/plotter.py
+++ apache/plotter.py
@@ -1,8 +1,28 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
+machine = "rpi"
+scheduler = "cfs_multicore"
+
+# Returns times in nanoseconds
+def read_scheduler(path):
+    with open(path) as file:
+        times = [float(line.rstrip().split("Total nanosec: ")[1]) for line in file]
+    return times
+
+def read_ping(path):
+    with open(path) as file:
+        times = [float(line.rstrip().split("time=")[1].split(" ms")[0])*1000000 for line in file]
+    return times
+
 
 filename = "cfs_multicore.tsv"
 
-df = pd.read_csv(filename,  sep='\t', header=0)
+http_times = read_scheduler(f"{machine}/{scheduler}")
+ping_times = read_ping(f"{machine}/ping_{scheduler}")
 
-plt.hist(df["wait"], bins=10000)
+print(http_times[1])
+print(ping_times[1])
+
+# in nanosec
+delta = 10000 # aka 0.01 ms
